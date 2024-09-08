9763c8ca48608203908a08aaf853da15e3eabfb0
Albert Tingson <tingsonalbert@gmail.com>
2024-09-08 08:57:55 +0800

    Quartz sync: Sep 8, 2024, 8:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Vollkorn SC",
+        header: "Vollkorn",
         body: "Inter",
         code: "Consolas",
       },
